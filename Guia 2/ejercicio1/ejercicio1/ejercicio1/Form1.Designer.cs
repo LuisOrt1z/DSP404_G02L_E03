95eafcf03c6947ea0375071bd4f05c44b1c6d711
emersongudiel98 <68823232+emersongudiel98@users.noreply.github.com>
2024-08-07 15:21:30 -0600

    Add files via upload

---
+++ Guia 2/ejercicio1/ejercicio1/ejercicio1/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace ejercicio1
+{
+    partial class frmFactorial
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblNumero = new Label();
+            lblFactorial = new Label();
+            txtNumero = new TextBox();
+            txtFactorial = new TextBox();
+            btnCalcular = new Button();
+            btnSalir = new Button();
+            SuspendLayout();
+            // 
+            // lblNumero
+            // 
+            lblNumero.AutoSize = true;
+            lblNumero.Location = new Point(252, 97);
+            lblNumero.Name = "lblNumero";
+            lblNumero.Size = new Size(54, 15);
+            lblNumero.TabIndex = 0;
+            lblNumero.Text = "Numero:";
+            // 
+            // lblFactorial
+            // 
+            lblFactorial.AutoSize = true;
+            lblFactorial.Location = new Point(443, 97);
+            lblFactorial.Name = "lblFactorial";
+            lblFactorial.Size = new Size(55, 15);
+            lblFactorial.TabIndex = 1;
+            lblFactorial.Text = "Factorial:";
+            // 
+            // txtNumero
+            // 
+            txtNumero.Location = new Point(220, 145);
+            txtNumero.Name = "txtNumero";
+            txtNumero.Size = new Size(100, 23);
+            txtNumero.TabIndex = 2;
+            txtNumero.TextChanged += txtNumero_TextChanged;
+            // 
+            // txtFactorial
+            // 
+            txtFactorial.Location = new Point(425, 145);
+            txtFactorial.Name = "txtFactorial";
+            txtFactorial.Size = new Size(100, 23);
+            txtFactorial.TabIndex = 3;
+            // 
+            // btnCalcular
+            // 
+            btnCalcular.Location = new Point(231, 199);
+            btnCalcular.Name = "btnCalcular";
+            btnCalcular.Size = new Size(75, 23);
+            btnCalcular.TabIndex = 4;
+            btnCalcular.Text = "Calcular";
+            btnCalcular.UseVisualStyleBackColor = true;
+            btnCalcular.Click += btnCalcular_Click;
+            // 
+            // btnSalir
+            // 
+            btnSalir.Location = new Point(443, 199);
+            btnSalir.Name = "btnSalir";
+            btnSalir.Size = new Size(75, 23);
+            btnSalir.TabIndex = 5;
+            btnSalir.Text = "Salir";
+            btnSalir.UseVisualStyleBackColor = true;
+            // 
+            // frmFactorial
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnSalir);
+            Controls.Add(btnCalcular);
+            Controls.Add(txtFactorial);
+            Controls.Add(txtNumero);
+            Controls.Add(lblFactorial);
+            Controls.Add(lblNumero);
+            Name = "frmFactorial";
+            Text = "Calculando el factorial de un numero";
+            Load += frmFactorial_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblNumero;
+        private Label lblFactorial;
+        private TextBox txtNumero;
+        private TextBox txtFactorial;
+        private Button btnCalcular;
+        private Button btnSalir;
+    }
+}
---
+++ Guia 2/ejercicio2/ejercicio2/ejercicio2/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace ejercicio2
+{
+    partial class frmCesar
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lbltexto = new System.Windows.Forms.Label();
+            this.lblllave = new System.Windows.Forms.Label();
+            this.txtLlave = new System.Windows.Forms.TextBox();
+            this.txtTexto = new System.Windows.Forms.TextBox();
+            this.grOpcion = new System.Windows.Forms.GroupBox();
+            this.rdbDesencriptar = new System.Windows.Forms.RadioButton();
+            this.rdbEncriptar = new System.Windows.Forms.RadioButton();
+            this.btnEncriptar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.lblresultado = new System.Windows.Forms.Label();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.grOpcion.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Location = new System.Drawing.Point(308, 28);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(159, 13);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Encriptamiento por codigo cesar";
+            // 
+            // lbltexto
+            // 
+            this.lbltexto.AutoSize = true;
+            this.lbltexto.Location = new System.Drawing.Point(151, 60);
+            this.lbltexto.Name = "lbltexto";
+            this.lbltexto.Size = new System.Drawing.Size(195, 13);
+            this.lbltexto.TabIndex = 1;
+            this.lbltexto.Text = "Ingrese el texto a encriptar/desencriptar";
+            // 
+            // lblllave
+            // 
+            this.lblllave.AutoSize = true;
+            this.lblllave.Location = new System.Drawing.Point(363, 63);
+            this.lblllave.Name = "lblllave";
+            this.lblllave.Size = new System.Drawing.Size(153, 13);
+            this.lblllave.TabIndex = 2;
+            this.lblllave.Text = "Llave (digite un numero entero)";
+            // 
+            // txtLlave
+            // 
+            this.txtLlave.Location = new System.Drawing.Point(522, 60);
+            this.txtLlave.Name = "txtLlave";
+            this.txtLlave.Size = new System.Drawing.Size(100, 20);
+            this.txtLlave.TabIndex = 3;
+            // 
+            // txtTexto
+            // 
+            this.txtTexto.Location = new System.Drawing.Point(137, 97);
+            this.txtTexto.Multiline = true;
+            this.txtTexto.Name = "txtTexto";
+            this.txtTexto.Size = new System.Drawing.Size(485, 127);
+            this.txtTexto.TabIndex = 4;
+            // 
+            // grOpcion
+            // 
+            this.grOpcion.Controls.Add(this.rdbDesencriptar);
+            this.grOpcion.Controls.Add(this.rdbEncriptar);
+            this.grOpcion.Location = new System.Drawing.Point(137, 251);
+            this.grOpcion.Name = "grOpcion";
+            this.grOpcion.Size = new System.Drawing.Size(327, 83);
+            this.grOpcion.TabIndex = 5;
+            this.grOpcion.TabStop = false;
+            this.grOpcion.Text = "Seleccione una opcion";
+            // 
+            // rdbDesencriptar
+            // 
+            this.rdbDesencriptar.AutoSize = true;
+            this.rdbDesencriptar.Location = new System.Drawing.Point(174, 29);
+            this.rdbDesencriptar.Name = "rdbDesencriptar";
+            this.rdbDesencriptar.Size = new System.Drawing.Size(85, 17);
+            this.rdbDesencriptar.TabIndex = 1;
+            this.rdbDesencriptar.Text = "Desencriptar";
+            this.rdbDesencriptar.UseVisualStyleBackColor = true;
+            this.rdbDesencriptar.CheckedChanged += new System.EventHandler(this.rdbDesencriptar_CheckedChanged);
+            // 
+            // rdbEncriptar
+            // 
+            this.rdbEncriptar.AutoSize = true;
+            this.rdbEncriptar.Checked = true;
+            this.rdbEncriptar.Location = new System.Drawing.Point(7, 29);
+            this.rdbEncriptar.Name = "rdbEncriptar";
+            this.rdbEncriptar.Size = new System.Drawing.Size(67, 17);
+            this.rdbEncriptar.TabIndex = 0;
+            this.rdbEncriptar.TabStop = true;
+            this.rdbEncriptar.Text = "Encriptar";
+            this.rdbEncriptar.UseVisualStyleBackColor = true;
+            this.rdbEncriptar.CheckedChanged += new System.EventHandler(this.rdbEncriptar_CheckedChanged);
+            // 
+            // btnEncriptar
+            // 
+            this.btnEncriptar.Location = new System.Drawing.Point(537, 251);
+            this.btnEncriptar.Name = "btnEncriptar";
+            this.btnEncriptar.Size = new System.Drawing.Size(75, 23);
+            this.btnEncriptar.TabIndex = 6;
+            this.btnEncriptar.Text = "Encriptar";
+            this.btnEncriptar.UseVisualStyleBackColor = true;
+            this.btnEncriptar.Click += new System.EventHandler(this.btnEncriptar_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(537, 282);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 7;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(537, 311);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 8;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // lblresultado
+            // 
+            this.lblresultado.AutoSize = true;
+            this.lblresultado.Location = new System.Drawing.Point(151, 337);
+            this.lblresultado.Name = "lblresultado";
+            this.lblresultado.Size = new System.Drawing.Size(35, 13);
+            this.lblresultado.TabIndex = 9;
+            this.lblresultado.Text = "label4";
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(154, 356);
+            this.txtResultado.Multiline = true;
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(433, 82);
+            this.txtResultado.TabIndex = 10;
+            // 
+            // frmCesar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.lblresultado);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnEncriptar);
+            this.Controls.Add(this.grOpcion);
+            this.Controls.Add(this.txtTexto);
+            this.Controls.Add(this.txtLlave);
+            this.Controls.Add(this.lblllave);
+            this.Controls.Add(this.lbltexto);
+            this.Controls.Add(this.lblTitulo);
+            this.Name = "frmCesar";
+            this.Text = "Encriptador/Desencriptador de Código";
+            this.grOpcion.ResumeLayout(false);
+            this.grOpcion.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lbltexto;
+        private System.Windows.Forms.Label lblllave;
+        private System.Windows.Forms.TextBox txtLlave;
+        private System.Windows.Forms.TextBox txtTexto;
+        private System.Windows.Forms.GroupBox grOpcion;
+        private System.Windows.Forms.RadioButton rdbDesencriptar;
+        private System.Windows.Forms.RadioButton rdbEncriptar;
+        private System.Windows.Forms.Button btnEncriptar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Label lblresultado;
+        private System.Windows.Forms.TextBox txtResultado;
+    }
+}
+
