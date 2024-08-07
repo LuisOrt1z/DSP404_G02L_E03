8ff2244347406045d1e3af6eb7d6b51d14f03752
emersongudiel98 <68823232+emersongudiel98@users.noreply.github.com>
2024-08-07 15:16:16 -0600

    Add files via upload

---
+++ GM171814_guia3/arreglounidimensional/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace arreglounidimensional
+{
+    public partial class frmarreglo : Form
+    {
+        int[] matriz = new int[5];
+        int i;
+        public frmarreglo()
+        {
+            InitializeComponent();
+        }
+
+        private void frmarreglo_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btAgregar_Click(object sender, EventArgs e)
+        {
+            if ( i <= 5) {
+                matriz[i] = Convert.ToInt16(txtNum.Text);
+                lsldesorden.Items.Add(matriz[i]);
+                i+= 1;
+                txtNum.Clear();
+                txtNum.Select();
+
+            }
+            else {
+                MessageBox.Show("NO SE PUEDE INGRESAR MAS DATOS", "ADVERTENCIA", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            
+
+        }
+
+        private void btOrdenar_Click(object sender, EventArgs e)
+        {
+            int j, k, count;
+            double valor;
+            int Tam = 5;
+
+            for (j = 0; j < Tam; j++)
+            {
+                for (k = 0; k < (Tam - 1); k++)
+                {
+                    if (matriz[k] > matriz[k + 1])
+                    {
+                        valor = matriz[k];
+                        matriz[k] = matriz[k + 1];
+                        matriz[k + 1] = Convert.ToInt16(valor);
+                    }
+                }
+            }
+
+            lstOrdenada.Items.Clear();
+            for (count = 0; count < Tam; count++)
+            {
+                lstOrdenada.Items.Add(matriz[count]);
+            }
+        }
+    }
+}
---
+++ GM171814_guia3/matiztresportres/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace matiztresportres
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbInformacion = new System.Windows.Forms.GroupBox();
+            this.txtEdad = new System.Windows.Forms.TextBox();
+            this.txtInfo = new System.Windows.Forms.TextBox();
+            this.cbtipoinfo = new System.Windows.Forms.ComboBox();
+            this.lbedad = new System.Windows.Forms.Label();
+            this.Lbdig_info = new System.Windows.Forms.Label();
+            this.Lbtipo_info = new System.Windows.Forms.Label();
+            this.gbMatriz = new System.Windows.Forms.GroupBox();
+            this.btSalir = new System.Windows.Forms.Button();
+            this.btPromedio = new System.Windows.Forms.Button();
+            this.btLimpiar = new System.Windows.Forms.Button();
+            this.btMostrar = new System.Windows.Forms.Button();
+            this.btAgregar = new System.Windows.Forms.Button();
+            this.gbMantenimiento = new System.Windows.Forms.GroupBox();
+            this.dgdatos = new System.Windows.Forms.DataGridView();
+            this.gbInformacion.SuspendLayout();
+            this.gbMatriz.SuspendLayout();
+            this.gbMantenimiento.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgdatos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gbInformacion
+            // 
+            this.gbInformacion.Controls.Add(this.txtEdad);
+            this.gbInformacion.Controls.Add(this.txtInfo);
+            this.gbInformacion.Controls.Add(this.cbtipoinfo);
+            this.gbInformacion.Controls.Add(this.lbedad);
+            this.gbInformacion.Controls.Add(this.Lbdig_info);
+            this.gbInformacion.Controls.Add(this.Lbtipo_info);
+            this.gbInformacion.Location = new System.Drawing.Point(33, 33);
+            this.gbInformacion.Name = "gbInformacion";
+            this.gbInformacion.Size = new System.Drawing.Size(324, 163);
+            this.gbInformacion.TabIndex = 0;
+            this.gbInformacion.TabStop = false;
+            this.gbInformacion.Text = "informacion";
+            // 
+            // txtEdad
+            // 
+            this.txtEdad.Location = new System.Drawing.Point(125, 125);
+            this.txtEdad.Name = "txtEdad";
+            this.txtEdad.Size = new System.Drawing.Size(105, 20);
+            this.txtEdad.TabIndex = 5;
+            // 
+            // txtInfo
+            // 
+            this.txtInfo.Location = new System.Drawing.Point(125, 82);
+            this.txtInfo.Name = "txtInfo";
+            this.txtInfo.Size = new System.Drawing.Size(157, 20);
+            this.txtInfo.TabIndex = 4;
+            // 
+            // cbtipoinfo
+            // 
+            this.cbtipoinfo.FormattingEnabled = true;
+            this.cbtipoinfo.Items.AddRange(new object[] {
+            "Nombre",
+            "Apellido"});
+            this.cbtipoinfo.Location = new System.Drawing.Point(125, 28);
+            this.cbtipoinfo.Name = "cbtipoinfo";
+            this.cbtipoinfo.Size = new System.Drawing.Size(157, 21);
+            this.cbtipoinfo.TabIndex = 3;
+            // 
+            // lbedad
+            // 
+            this.lbedad.AutoSize = true;
+            this.lbedad.Location = new System.Drawing.Point(19, 125);
+            this.lbedad.Name = "lbedad";
+            this.lbedad.Size = new System.Drawing.Size(32, 13);
+            this.lbedad.TabIndex = 2;
+            this.lbedad.Text = "Edad";
+            // 
+            // Lbdig_info
+            // 
+            this.Lbdig_info.AutoSize = true;
+            this.Lbdig_info.Location = new System.Drawing.Point(19, 82);
+            this.Lbdig_info.Name = "Lbdig_info";
+            this.Lbdig_info.Size = new System.Drawing.Size(102, 13);
+            this.Lbdig_info.TabIndex = 1;
+            this.Lbdig_info.Text = "Digite la informacion";
+            // 
+            // Lbtipo_info
+            // 
+            this.Lbtipo_info.AutoSize = true;
+            this.Lbtipo_info.Location = new System.Drawing.Point(19, 31);
+            this.Lbtipo_info.Name = "Lbtipo_info";
+            this.Lbtipo_info.Size = new System.Drawing.Size(100, 13);
+            this.Lbtipo_info.TabIndex = 0;
+            this.Lbtipo_info.Text = "Tipo de informacion";
+            // 
+            // gbMatriz
+            // 
+            this.gbMatriz.Controls.Add(this.btSalir);
+            this.gbMatriz.Controls.Add(this.btPromedio);
+            this.gbMatriz.Controls.Add(this.btLimpiar);
+            this.gbMatriz.Controls.Add(this.btMostrar);
+            this.gbMatriz.Controls.Add(this.btAgregar);
+            this.gbMatriz.Location = new System.Drawing.Point(430, 33);
+            this.gbMatriz.Name = "gbMatriz";
+            this.gbMatriz.Size = new System.Drawing.Size(324, 163);
+            this.gbMatriz.TabIndex = 1;
+            this.gbMatriz.TabStop = false;
+            this.gbMatriz.Text = "Matriz";
+            // 
+            // btSalir
+            // 
+            this.btSalir.BackColor = System.Drawing.Color.DarkSalmon;
+            this.btSalir.Location = new System.Drawing.Point(25, 123);
+            this.btSalir.Name = "btSalir";
+            this.btSalir.Size = new System.Drawing.Size(75, 23);
+            this.btSalir.TabIndex = 4;
+            this.btSalir.Text = "Salir";
+            this.btSalir.UseVisualStyleBackColor = false;
+            // 
+            // btPromedio
+            // 
+            this.btPromedio.BackColor = System.Drawing.Color.Khaki;
+            this.btPromedio.Location = new System.Drawing.Point(144, 82);
+            this.btPromedio.Name = "btPromedio";
+            this.btPromedio.Size = new System.Drawing.Size(75, 23);
+            this.btPromedio.TabIndex = 3;
+            this.btPromedio.Text = "Promedio";
+            this.btPromedio.UseVisualStyleBackColor = false;
+            this.btPromedio.Click += new System.EventHandler(this.btPromedio_Click);
+            // 
+            // btLimpiar
+            // 
+            this.btLimpiar.BackColor = System.Drawing.Color.SandyBrown;
+            this.btLimpiar.Location = new System.Drawing.Point(25, 77);
+            this.btLimpiar.Name = "btLimpiar";
+            this.btLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btLimpiar.TabIndex = 2;
+            this.btLimpiar.Text = "Limpiar matriz";
+            this.btLimpiar.UseVisualStyleBackColor = false;
+            this.btLimpiar.Click += new System.EventHandler(this.btLimpiar_Click);
+            // 
+            // btMostrar
+            // 
+            this.btMostrar.BackColor = System.Drawing.Color.Lime;
+            this.btMostrar.Location = new System.Drawing.Point(144, 31);
+            this.btMostrar.Name = "btMostrar";
+            this.btMostrar.Size = new System.Drawing.Size(75, 23);
+            this.btMostrar.TabIndex = 1;
+            this.btMostrar.Text = "Mostrar matriz";
+            this.btMostrar.UseVisualStyleBackColor = false;
+            this.btMostrar.Click += new System.EventHandler(this.btMostrar_Click);
+            // 
+            // btAgregar
+            // 
+            this.btAgregar.BackColor = System.Drawing.Color.Red;
+            this.btAgregar.Location = new System.Drawing.Point(25, 28);
+            this.btAgregar.Name = "btAgregar";
+            this.btAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btAgregar.TabIndex = 0;
+            this.btAgregar.Text = "Agregar";
+            this.btAgregar.UseVisualStyleBackColor = false;
+            this.btAgregar.Click += new System.EventHandler(this.btAgregar_Click);
+            // 
+            // gbMantenimiento
+            // 
+            this.gbMantenimiento.Controls.Add(this.dgdatos);
+            this.gbMantenimiento.Location = new System.Drawing.Point(33, 218);
+            this.gbMantenimiento.Name = "gbMantenimiento";
+            this.gbMantenimiento.Size = new System.Drawing.Size(721, 202);
+            this.gbMantenimiento.TabIndex = 1;
+            this.gbMantenimiento.TabStop = false;
+            this.gbMantenimiento.Text = "Mantenimientos";
+            // 
+            // dgdatos
+            // 
+            this.dgdatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgdatos.Location = new System.Drawing.Point(22, 34);
+            this.dgdatos.Name = "dgdatos";
+            this.dgdatos.Size = new System.Drawing.Size(670, 147);
+            this.dgdatos.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Chocolate;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gbMantenimiento);
+            this.Controls.Add(this.gbMatriz);
+            this.Controls.Add(this.gbInformacion);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.gbInformacion.ResumeLayout(false);
+            this.gbInformacion.PerformLayout();
+            this.gbMatriz.ResumeLayout(false);
+            this.gbMantenimiento.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgdatos)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbInformacion;
+        private System.Windows.Forms.TextBox txtEdad;
+        private System.Windows.Forms.TextBox txtInfo;
+        private System.Windows.Forms.ComboBox cbtipoinfo;
+        private System.Windows.Forms.Label lbedad;
+        private System.Windows.Forms.Label Lbdig_info;
+        private System.Windows.Forms.Label Lbtipo_info;
+        private System.Windows.Forms.GroupBox gbMatriz;
+        private System.Windows.Forms.Button btSalir;
+        private System.Windows.Forms.Button btPromedio;
+        private System.Windows.Forms.Button btLimpiar;
+        private System.Windows.Forms.Button btMostrar;
+        private System.Windows.Forms.Button btAgregar;
+        private System.Windows.Forms.GroupBox gbMantenimiento;
+        private System.Windows.Forms.DataGridView dgdatos;
+    }
+}
+
