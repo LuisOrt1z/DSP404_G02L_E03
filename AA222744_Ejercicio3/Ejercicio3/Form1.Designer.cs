69e8022d3d0603beec0e99c5b0010495d3e27283
Fernando-Anzora <101297615+Fernando-Anzora@users.noreply.github.com>
2024-08-08 22:14:09 -0600

    Diseño y filtros
    Creación de diseño, implementación de botón agregar tarea y función ordenar lista por filtros

---
+++ AA222744_Ejercicio3/Ejercicio3/Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace Ejercicio3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.dtpFecha = new System.Windows.Forms.DateTimePicker();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnDesmarcar = new System.Windows.Forms.Button();
+            this.lstCompletadas = new System.Windows.Forms.ListBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnCompletada = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lstTareas = new System.Windows.Forms.ListBox();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmbFiltro = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(21, 56);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(235, 22);
+            this.txtNombre.TabIndex = 0;
+            // 
+            // dtpFecha
+            // 
+            this.dtpFecha.Location = new System.Drawing.Point(21, 128);
+            this.dtpFecha.Name = "dtpFecha";
+            this.dtpFecha.Size = new System.Drawing.Size(266, 22);
+            this.dtpFecha.TabIndex = 1;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(72, 169);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(136, 32);
+            this.btnAgregar.TabIndex = 2;
+            this.btnAgregar.Text = "Agregar tarea";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btnAgregar);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.dtpFecha);
+            this.groupBox1.Controls.Add(this.txtNombre);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(301, 217);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Nueva tarea";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Fecha limite";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre de la tarea";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnDesmarcar);
+            this.groupBox2.Controls.Add(this.lstCompletadas);
+            this.groupBox2.Location = new System.Drawing.Point(12, 235);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(301, 208);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Tareas completadas";
+            // 
+            // btnDesmarcar
+            // 
+            this.btnDesmarcar.Location = new System.Drawing.Point(36, 164);
+            this.btnDesmarcar.Name = "btnDesmarcar";
+            this.btnDesmarcar.Size = new System.Drawing.Size(220, 32);
+            this.btnDesmarcar.TabIndex = 1;
+            this.btnDesmarcar.Text = "Desmarcar como completada";
+            this.btnDesmarcar.UseVisualStyleBackColor = true;
+            // 
+            // lstCompletadas
+            // 
+            this.lstCompletadas.FormattingEnabled = true;
+            this.lstCompletadas.ItemHeight = 16;
+            this.lstCompletadas.Location = new System.Drawing.Point(11, 21);
+            this.lstCompletadas.Name = "lstCompletadas";
+            this.lstCompletadas.Size = new System.Drawing.Size(276, 132);
+            this.lstCompletadas.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.cmbFiltro);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.btnCompletada);
+            this.groupBox3.Controls.Add(this.btnEditar);
+            this.groupBox3.Controls.Add(this.btnEliminar);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.lstTareas);
+            this.groupBox3.Location = new System.Drawing.Point(335, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(411, 431);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Lista de tareas";
+            // 
+            // btnCompletada
+            // 
+            this.btnCompletada.Location = new System.Drawing.Point(215, 384);
+            this.btnCompletada.Name = "btnCompletada";
+            this.btnCompletada.Size = new System.Drawing.Size(185, 32);
+            this.btnCompletada.TabIndex = 8;
+            this.btnCompletada.Text = "Marcar como completada";
+            this.btnCompletada.UseVisualStyleBackColor = true;
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Location = new System.Drawing.Point(112, 384);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(97, 32);
+            this.btnEditar.TabIndex = 7;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(17, 384);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(89, 32);
+            this.btnEliminar.TabIndex = 6;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 360);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Opciones";
+            // 
+            // lstTareas
+            // 
+            this.lstTareas.FormattingEnabled = true;
+            this.lstTareas.ItemHeight = 16;
+            this.lstTareas.Location = new System.Drawing.Point(17, 58);
+            this.lstTareas.Name = "lstTareas";
+            this.lstTareas.Size = new System.Drawing.Size(383, 292);
+            this.lstTareas.TabIndex = 0;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(332, 454);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(86, 32);
+            this.btnSalir.TabIndex = 0;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(146, 28);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Filtrar por";
+            // 
+            // cmbFiltro
+            // 
+            this.cmbFiltro.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbFiltro.FormattingEnabled = true;
+            this.cmbFiltro.Items.AddRange(new object[] {
+            "Ninguno",
+            "Nombre(A - Z)",
+            "Nombre(Z- A)",
+            "Fecha(Mas cercanas)",
+            "Fecha(Mas leganas)"});
+            this.cmbFiltro.Location = new System.Drawing.Point(215, 25);
+            this.cmbFiltro.Name = "cmbFiltro";
+            this.cmbFiltro.Size = new System.Drawing.Size(185, 24);
+            this.cmbFiltro.TabIndex = 10;
+            this.cmbFiltro.SelectedIndexChanged += new System.EventHandler(this.cmbFiltro_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(759, 498);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Tareas";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.DateTimePicker dtpFecha;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnDesmarcar;
+        private System.Windows.Forms.ListBox lstCompletadas;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnCompletada;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox lstTareas;
+        private System.Windows.Forms.ComboBox cmbFiltro;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ AA222744_Ejercicio3/Ejercicio3/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public class Tarea
+        {
+            public string Nombre { get; set; }
+            public DateTime Fecha { get; set; }
+            public bool Completa { get; set; }
+            public void Datos(string nombre, DateTime fechaLimite)
+            {
+                Nombre = nombre;
+                Fecha = fechaLimite;
+                Completa = false;
+            }
+        }
+        public List<Tarea> ListaTareas = new List<Tarea>();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbFiltro.SelectedIndex = 0;
+        }
+        private void ActualizarListas(int filtro)
+        {
+            lstTareas.Items.Clear();
+            lstCompletadas.Items.Clear();
+            var Completas = ListaTareas.Where(n => n.Completa == true).ToList();
+            switch (filtro)
+            {
+                case 0:
+                    var Pendientes = ListaTareas.Where(n => n.Completa == false).ToList();
+                    if (Pendientes.Any())
+                    {
+                        foreach (Tarea Lista in Pendientes)
+                        {
+                            lstTareas.Items.Add(Lista.Nombre + " <|> " + Lista.Fecha);
+                        }
+                    }
+                    break;
+                case 1:
+                    var AlfabeticoA_Z = ListaTareas.OrderBy(n => n.Nombre).ToList();
+                    if (AlfabeticoA_Z.Any())
+                    {
+                        foreach (Tarea Lista in AlfabeticoA_Z)
+                        {
+                            lstTareas.Items.Add(Lista.Nombre + " <|> " + Lista.Fecha);
+                        }
+                    }
+                    break;
+                case 2:
+                    var AlfabeticoZ_A = ListaTareas.OrderByDescending(n => n.Nombre).ToList();
+                    if (AlfabeticoZ_A.Any())
+                    {
+                        foreach (Tarea Lista in AlfabeticoZ_A)
+                        {
+                            lstTareas.Items.Add(Lista.Nombre + " <|> " + Lista.Fecha);
+                        }
+                    }
+                    break;
+                case 3:
+                    var FechaCercana = ListaTareas.OrderBy(n => n.Fecha).ToList();
+                    if (FechaCercana.Any())
+                    {
+                        foreach (Tarea Lista in FechaCercana)
+                        {
+                            lstTareas.Items.Add(Lista.Nombre + " <|> " + Lista.Fecha);
+                        }
+                    }
+                    break;
+                case 4:
+                    var FechaLegana = ListaTareas.OrderByDescending(n => n.Fecha).ToList();
+                    if (FechaLegana.Any())
+                    {
+                        foreach (Tarea Lista in FechaLegana)
+                        {
+                            lstTareas.Items.Add(Lista.Nombre + " <|> " + Lista.Fecha);
+                        }
+                    }
+                    break;
+            }
+
+            if (Completas.Any())
+            {
+                foreach (Tarea Lista in Completas)
+                {
+                    lstCompletadas.Items.Add(Lista.Nombre + " <|> " + Lista.Fecha);
+                }
+            }
+
+            
+
+        }
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            Tarea NuevaTarea = new Tarea();
+            NuevaTarea.Datos(txtNombre.Text,dtpFecha.Value);
+            ListaTareas.Add(NuevaTarea);
+            ActualizarListas(TipoFiltro());
+
+        }
+
+        private void cmbFiltro_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ActualizarListas(TipoFiltro());
+        }
+        private int TipoFiltro()
+        {
+            string filtro = Convert.ToString(cmbFiltro.SelectedItem);
+            switch (filtro)
+            {
+                case "Ninguno":
+                    return 0;
+                case "Nombre(A - Z)":
+                    return 1;
+                case "Nombre(Z- A)":
+                    return 2;
+                case "Fecha(Mas cercanas)":
+                    return 3;
+                case "Fecha(Mas leganas)":
+                    return 4;
+            }
+            return 0;
+        }
+    }
+}
