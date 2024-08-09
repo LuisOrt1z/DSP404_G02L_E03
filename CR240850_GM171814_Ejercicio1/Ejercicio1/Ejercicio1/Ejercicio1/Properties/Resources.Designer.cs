539584c0047c52956b3955d0c6f9962288e051a4
emersongudiel98 <68823232+emersongudiel98@users.noreply.github.com>
2024-08-09 11:41:57 -0600

    inicio de login

--- CR240850_GM171814_Ejercicio1/Ejercicio1/Ejercicio1/Ejercicio1/Properties/Resources.Designer.cs
+++ CR240850_GM171814_Ejercicio1/Ejercicio1/Ejercicio1/Ejercicio1/Properties/Resources.Designer.cs
@@ -79,5 +79,15 @@ namespace Ejercicio1.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Login {
+            get {
+                object obj = ResourceManager.GetObject("Login", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
