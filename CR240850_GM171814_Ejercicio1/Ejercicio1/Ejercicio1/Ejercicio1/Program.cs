1e4612f95c45984fab37c43ad53e3882e62f37dd
emersongudiel98 <68823232+emersongudiel98@users.noreply.github.com>
2024-08-09 00:14:56 -0600

    empezando login

--- CR240850_GM171814_Ejercicio1/Ejercicio1/Ejercicio1/Ejercicio1/Program.cs
+++ CR240850_GM171814_Ejercicio1/Ejercicio1/Ejercicio1/Ejercicio1/Program.cs
@@ -17,6 +17,7 @@ namespace Ejercicio1
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            Application.Run(new Login());
         }
     }
 }
