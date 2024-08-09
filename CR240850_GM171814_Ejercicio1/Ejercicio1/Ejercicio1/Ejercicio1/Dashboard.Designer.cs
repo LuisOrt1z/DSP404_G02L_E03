f6bd5952c0162bf873a9de7b1e892e867e198fd3
LuisCruz991 <83253984+LuisCruz991@users.noreply.github.com>
2024-08-08 23:44:23 -0600

    Arreglos dashboard parte1

---
+++ CR240850_GM171814_Ejercicio1/Ejercicio1/Ejercicio1/Ejercicio1/Dashboard.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace Ejercicio1
+{
+    partial class Dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Dashboard
+            // 
+            this.ClientSize = new System.Drawing.Size(697, 517);
+            this.Name = "Dashboard";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabEntretenimiento;
+        private System.Windows.Forms.TabPage tabLibros;
+        private System.Windows.Forms.TabPage tabProgra;
+        private System.Windows.Forms.TabPage tabGraficas;
+        private System.Windows.Forms.Label txtNombre;
+    }
+}
