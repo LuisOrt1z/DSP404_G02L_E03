eba7da728a116038d8e11084792cdb48918b7d50
Fernando-Anzora <101297615+Fernando-Anzora@users.noreply.github.com>
2024-08-09 23:05:58 -0600

    2 Discusiones de resultados

---
+++ AA222744_guia_02/Discucion de resultados/Problema3/Problema3/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Problema3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<double> numeros = new List<double>();
+        public static Boolean ValidaNum(string valor)
+        {
+            double result;
+            return double.TryParse(valor, out result);
+        }
+        public void ActualizarListas()
+        {
+            lstNumeros.Items.Clear();
+            int index = 1;
+            foreach (double numero in numeros)
+            {
+                lstNumeros.Items.Add(index + ". Numero: " + numero);
+                index++;
+            }
+            index = index - 1;
+
+            lstResultados.Items.Clear();
+            var negativos = numeros.Where(n => n < 0).ToList();
+            var positivos = numeros.Where(n => n > 0).ToList();
+            int CantidadPositivos = numeros.Count(n => n > 0);
+            int CantidadNegativos = numeros.Count(n => n < 0);
+            int CantidadCeros = numeros.Count(n => n == 0);
+            if (negativos.Any())
+            {
+                double MayorNegativo = negativos.Max();
+                double promedioNegativo = negativos.Average();
+                lstResultados.Items.Add("El mayor de los negativos es: " + MayorNegativo);
+                lstResultados.Items.Add("El promedio de los negativos es: " + promedioNegativo.ToString("0.00"));
+            }
+            else
+            {
+                lstResultados.Items.Add("No hay numeros negativos en la lista");
+            }
+
+            if (positivos.Any())
+            {
+                lstResultados.Items.Add("La cantidad de numeros positivos es: " + CantidadPositivos);
+            }
+            else
+            {
+                lstResultados.Items.Add("No hay numeros positivos en la lista");
+            }
+            double porcentajePositivo = ((double)CantidadPositivos / index)*100;
+            double porcentajeNegativo = ((double)CantidadNegativos / index)*100;
+            lstResultados.Items.Add("El porcentaje de los positivos es: " + porcentajePositivo.ToString("0.00") + "%");
+            lstResultados.Items.Add("El porcentaje de los Negativos es: " + porcentajeNegativo.ToString("0.00") + "%");
+            if(CantidadCeros > 0)
+            {
+                lstResultados.Items.Add("La cantidad de 0 en la lista es: " + CantidadCeros);
+            }
+            else
+            {
+                lstResultados.Items.Add("No hay ningun 0 en la lista");
+            }
+            
+        }
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            if (!(ValidaNum(txtNumero.Text)))
+            {
+                MessageBox.Show("El valor ingresado no es un numero", "Error:", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                txtNumero.Focus();
+                txtNumero.Text = "";
+                txtNumero.BackColor = Color.Red;
+                return;
+            }
+            double NumeroIngresado = Convert.ToDouble(txtNumero.Text);
+            if (!(NumeroIngresado >= -40 && NumeroIngresado <= 45))
+            {
+                MessageBox.Show("Solo se permiten numeros del rango -40 a 45", "Informacion", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtNumero.Focus();
+                txtNumero.Text = "";
+                txtNumero.BackColor = Color.Red;
+                return;
+            }
+            numeros.Add(NumeroIngresado);
+            ActualizarListas();
+        }
+
+        private void txtNumero_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            txtNumero.BackColor = Color.White;
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            numeros.Clear();
+            lstNumeros.Items.Clear();
+            lstResultados.Items.Clear();
+            txtNumero.Text = "";
+            txtNumero.Focus();
+
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ AA222744_guia_02/Discucion de resultados/Problema4/Problema4/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Problema4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static Boolean ValidaNum(string valor)
+        {
+            double result;
+            return double.TryParse(valor, out result);
+        }
+        public void CambioMoneda(string monedaSelecionada, int index)
+        {
+            switch (monedaSelecionada)
+            {
+                case "Dólar estadounidense":
+                    monedaSelecionada = "(USD)";
+                    break;
+                case "Euro":
+                    monedaSelecionada = "(EUR)";
+                    break;
+                case "Yen japonés":
+                    monedaSelecionada = "(JPY)";
+                    break;
+                case "Libra esterlina":
+                    monedaSelecionada = "(GBP)";
+                    break;
+                case "Franco suizo":
+                    monedaSelecionada = "(CHF)";
+                    break;
+            }
+
+            if (index == 0)
+            {
+                lblNativa.Text = monedaSelecionada;
+            }
+            else
+            {
+                lblEstranjera.Text = monedaSelecionada;
+            }
+        }
+        public void ConvertirMondeda(string monedaNativa,string MonedaExtranjera, double cantidad)
+        {
+            const double USD_A_EUR = 0.9142;
+            const double USD_A_JPY = 156.1341;
+            const double USD_A_GBP = 0.7688;
+            const double USD_A_CHF = 0.8830;
+
+            double NativaSelecionada = 0, ExtranjeraSelecionada = 0;
+
+            switch (monedaNativa)
+            {
+                case "(USD)":
+                    NativaSelecionada = 1;
+                    break;
+                case "(EUR)":
+                    NativaSelecionada = 1 / USD_A_EUR;
+                    break;
+                case "(JPY)":
+                    NativaSelecionada = 1 / USD_A_JPY;
+                    break;
+                case "(GBP)":
+                    NativaSelecionada = 1 / USD_A_GBP;
+                    break;
+                case "(CHF)":
+                    NativaSelecionada = 1 / USD_A_CHF;
+                    break;
+            }
+            switch (MonedaExtranjera)
+            {
+                case "(USD)":
+                    ExtranjeraSelecionada = 1;
+                    break;
+                case "(EUR)":
+                    ExtranjeraSelecionada = USD_A_EUR;
+                    break;
+                case "(JPY)":
+                    ExtranjeraSelecionada = USD_A_JPY;
+                    break;
+                case "(GBP)":
+                    ExtranjeraSelecionada = USD_A_GBP;
+                    break;
+                case "(CHF)":
+                    ExtranjeraSelecionada = USD_A_CHF;
+                    break;
+            }
+
+            double conversion = (cantidad * NativaSelecionada) * ExtranjeraSelecionada;
+            lblResultado.Text = "Resultado: " + cantidad + " " + monedaNativa +" = " + conversion.ToString("0.0000") + " " + MonedaExtranjera;
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblNativa.Text = "()";
+            lblEstranjera.Text = "()";
+        }
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            if (!(ValidaNum(txtCantidad.Text)))
+            {
+                MessageBox.Show("El valor ingresado no es un numero", "Error:", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                txtCantidad.Focus();
+                txtCantidad.Text = "";
+                txtCantidad.BackColor = Color.Red;
+                return;
+            }
+            double cantidad = Convert.ToDouble(txtCantidad.Text);
+            if ((cmbNativa.SelectedIndex == -1) || (cmbExtranjera.SelectedIndex == -1))
+            {
+                MessageBox.Show("Selecione una opcion de las divisas disponibles", "Informacion", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            if (cmbNativa.SelectedIndex == cmbExtranjera.SelectedIndex)
+            {
+                MessageBox.Show("Las divisas selecionadas son iguales, No necesitan conversion", "Informacion", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            ConvertirMondeda(lblNativa.Text,lblEstranjera.Text, cantidad);
+
+
+
+        }
+
+        private void btnIntercambio_Click(object sender, EventArgs e)
+        {
+            int IndexTemporal = cmbNativa.SelectedIndex;
+            cmbNativa.SelectedIndex = cmbExtranjera.SelectedIndex;
+            cmbExtranjera.SelectedIndex = IndexTemporal;
+        }
+
+        private void cmbNativa_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string MonedaSelecionada = Convert.ToString(cmbNativa.SelectedItem);
+            CambioMoneda(MonedaSelecionada, 0);
+        }
+
+        private void cmbExtranjera_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string MonedaSelecionada = Convert.ToString(cmbExtranjera.SelectedItem);
+            CambioMoneda(MonedaSelecionada, 1);
+        }
+
+        private void txtCantidad_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            txtCantidad.BackColor = Color.White;
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
