23f42d7aa249c7f22af0562fb4c4e0191b6dbffd
Fernando-Anzora <101297615+Fernando-Anzora@users.noreply.github.com>
2024-09-21 15:33:10 -0600

    Add files via upload

---
+++ CajeroWeb/CajeroWeb/Controllers/SaldoController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CajeroWeb.Controllers
+{
+    public class SaldoController : Controller
+    {
+        // GET: Saldo
+        public ActionResult Saldo(string sNumeroTarjeta, double sSaldo)
+        {
+            ViewBag.sNumeroTarjeta = sNumeroTarjeta;
+            ViewBag.sSaldo = sSaldo;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Saldo(string sNumeroTarjeta, double sSaldo, double cantidad)
+        {
+            return Redirect("~/Menu/Menu?sNumeroTarjeta=" + sNumeroTarjeta + "&sSaldo=" + sSaldo);
+        }
+    }
+}
---
+++ CajeroWeb/CajeroWeb/Models/transaccion.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CajeroWeb.Models
+{
+    public class transaccion
+    {
+        private String sNumeroTarjeta;
+        private double fSaldo;
+
+        public transaccion(String tarjeta)
+        {
+            sNumeroTarjeta = tarjeta;
+            fSaldo = 500;
+        }
+        public transaccion(String tarjeta, double saldo)
+        {
+            sNumeroTarjeta = tarjeta;
+            fSaldo = saldo;
+        }
+        //metodo get
+        public double getSaldo()
+        {
+            return fSaldo;
+        }
+        public void setSaldo(double saldo)
+        {
+            fSaldo = saldo;
+        }
+        public String getNumTarjeta()
+        {
+            return sNumeroTarjeta;
+        }
+        public void setNumTarjeta(String tarjeta)
+        {
+            sNumeroTarjeta = tarjeta;
+        }
+        public void transferenciaNacional(double monto)
+        {
+            fSaldo = fSaldo - monto;
+        }
+        public void transferenciaInternacional(double monto)
+        {
+            fSaldo = fSaldo - monto;
+        }
+    }
+}
