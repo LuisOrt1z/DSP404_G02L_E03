cf23b371be8ec26da4426d9bb6906d922f4ad05e
LuisOrt1z <OH242206@alumno.udb.edu.sv>
2024-09-23 19:40:46 -0600

    avance y conexion de db con login y registro

---
+++ The Temper Trap/The Temper Trap/Models/ModeloEmpleados.cs
@@ -0,0 +1,12 @@
+﻿namespace The_Temper_Trap.Models
+{
+    public class Empleado
+    {
+        public int id_empleado { get; set; } // Puedes dejarlo sin usar si es autoincremental
+        public string nombre { get; set; }
+        public string puesto { get; set; }
+        public string telefono { get; set; }
+        public string email { get; set; }
+        public string contrasena { get; set; }
+    }
+}
