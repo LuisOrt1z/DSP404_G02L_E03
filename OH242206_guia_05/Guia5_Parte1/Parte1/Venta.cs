4057c2f3b8f1e9c7c4e2bcc96704b328a989916c
LuisOrt1z <OH242206@alumno.udb.edu.sv>
2024-10-27 18:45:37 -0600

    subida guia05

---
+++ OH242206_guia_05/Guia5_Parte1/Parte1/Venta.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Parte1
+{
+    public class Venta
+    {
+        private string cliente;
+        private DateTime fecha;
+        private List<Producto> productos;
+
+        public Venta(string cliente)
+        {
+            this.cliente = cliente;
+            this.fecha = DateTime.Now;
+            this.productos = new List<Producto>();
+        }
+
+        public List<Producto> Productos { get => productos;}
+
+        public void AgregarProducto(Producto producto)
+        {
+            productos.Add(producto);
+        }
+
+        public decimal Total()
+        {
+            decimal total = 0;
+            foreach (Producto producto in Productos)
+            {
+                total += producto.Valor;
+            }
+            return total;
+        }
+    }
+}
